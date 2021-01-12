0b12894d39e304fade99bb2269d1a639a46f9973
Joseph <josefweel@gmail.com>
2021-01-12 12:35:12 -0500

    Init

---
+++ Hex/Core.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Hex
+{
+    public class Core : Game
+    {
+        #region Constructors
+
+        public Core()
+        {
+            this.Graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+            
+        #endregion
+
+        #region Properties
+        
+        protected GraphicsDeviceManager Graphics;
+        protected SpriteBatch SpriteBatch;
+            
+        #endregion
+
+        #region Protected Methods
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.SpriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+            
+        #endregion
+    }
+}
---
+++ Hex/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hex
+{
+    public static class Program
+    {
+        [System.STAThread]
+        private static void Main()
+        {
+            using var core = new Core();
+            core.Run();
+        }
+    }
+}
